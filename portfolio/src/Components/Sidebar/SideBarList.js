4d782b179d4c36edcfa1cadfc803cf337c7dc4e2
Yurii <yuriydorosh2005@gmail.com>
2023-10-10 20:38:58 +0100

    UPDATE: SideBarList(ternary operator)

--- portfolio/src/Components/Sidebar/SideBarList.js
+++ portfolio/src/Components/Sidebar/SideBarList.js
@@ -2,19 +2,24 @@ import React from 'react'
 import './SideBarList.css'
 import profilepicture from '../../Image/Profile.jpg'
 
-const SideBarList = () => {
+const SideBarList = ({expandSideBar}) => {
   return (
     <React.Fragment>
 
+      {expandSideBar ? (
         <div className='navbar-items'>
-
-            <div className='sidebar-profile-pic'>
-
-                <img src={profilepicture} alt=''></img>
-
-            </div>
+          <div className='sidebar-profile-pic'>
+            <img src={profilepicture} alt='profile photo'></img>
+          </div>
+        </div>
+      ): 
+      (
+        <div>
 
         </div>
+      )}
+
+
 
     </React.Fragment>
   )
