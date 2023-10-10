e227c974ce05f85251db882f742b3de36f6fcd09
Yurii <yuriydorosh2005@gmail.com>
2023-10-10 17:57:56 +0300

    ADD: Components/Sidebar

---
+++ portfolio/src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import "./Sidebar.css"
+import Home from '../Home/Home'
+import { BsChevronLeft, } from "react-icons/bs";
+import { BsChevronRight, } from "react-icons/bs";
+
+const Sidebar = () => {
+
+    const [expandSideBar, setExtandSideBar] = useState(true)
+    const handleClick = () => {
+        setExtandSideBar(!expandSideBar)
+    }
+
+
+  return (
+    <div className="container-fluid sidebar-section">
+
+        <div className={expandSideBar? 'sidebar-expand sidebar': 'sidebar'}>
+
+            <div className='icon-for-sidebar-expand-and-collapse'>
+                <p onClick={handleClick}>
+                    {expandSideBar ? (
+                        <BsChevronLeft size={30}/>
+                    ): (
+                        <BsChevronRight size={30}/>
+                    ) }
+                </p>
+            </div>
+
+
+        </div>
+
+
+        <div className='container'>
+            <Home/>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
