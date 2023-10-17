7eba5a5f578b1e26e17b60d77b90d3862aed591b
Yurii <yuriydorosh2005@gmail.com>
2023-10-17 22:14:56 +0100

    UPDATE: App.js(Education)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import Sidebar from './Components/Sidebar/Sidebar';
 import About from "./Components/About/About";
 import TechStack from './Components/TechStack/TechStack';
 import Project from "./Components/Project/Project";
+import Education from "./Components/Education/Education";
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <Sidebar></Sidebar>
     <About />
     <TechStack />
+    <Education />
     <Project />
     </>
   )
