3431b9fbf44a2bd707f7cb22d3da1e766481b66e
Yurii <yuriydorosh2005@gmail.com>
2023-10-17 16:41:24 +0100

    UPDATE: Home.js(deleted useless import)

--- portfolio/src/Components/Home/Home.js
+++ portfolio/src/Components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Home.css";
 import Typewriter from "typewriter-effect";
-// import MyCV from "./cv.pdf";
 import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 import { Link } from "react-scroll";
 
